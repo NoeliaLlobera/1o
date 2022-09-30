5bb731a35dc9a3d28830b95719488f9caa0c1e22
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-09-30 15:42:09 +0200

    primer

---
+++ grafics.js
@@ -0,0 +1,55 @@
+// // var chartVars = "KoolOnLoadCallFunction=chartReadyHandler";
+// //                 KoolChart.create("chart", "chartHolder", chartVars, "100%", "100%");
+// //                 function chartReadyHandler(id) {
+// //                 document.getElementById(id).setLayout(layoutStr);
+// //                 document.getElementById(id).setData(chartData);
+// //                 }
+
+// //                 var layoutStr =
+// //                 '<KoolChart backgroundColor="" padding="22">'
+// //                     +'<Options>'
+// //                     +'<Caption text=""/>'
+// //                     +'</Options>'
+// //                     +'<PictorialChart type="count" typeMode="form" order="sort" maxPicNumber="100" linePicNumber="20" '
+// //                     +'picHGap="180" picVGap="140" direction="up" picXOffset="82" '
+// //                     +'duration="1.5" textVLength="40" textPosition="down" descXOffset="300" showValueImage="false" '
+// //                     +'showValue="true" valueFontSize="20" valueFontWeight="bold" valueYOffset="17" '
+// //                     +'showText="true" textFontSize="14" textFontWeight="600" textColor="#333333" textXOffset="16" textYOffset="22" textAlign="left" itemHGap="16" '
+// //                     +'showLegend="true" showMultiItems="true" legendTextFontSize="14" legendTextColor="#333333" legendXOffset="355" legendYOffset="11" legendGap="29" '
+// //                     +'legendImageWidth="10" legendImageHeight="15" legendTextXOffset="6" legendItemHGap="15">'
+// //                     +'</PictorialChart>'
+// //                 +'</KoolChart>';
+
+// //                 var chartData =
+// //                 [{"name":"Altres", "value":82, "legendText":"Altres %", "color":"#9bd6d9", "url":"https://www.koolchart.com/demo/KoolChart/Assets/Images/man.svg"},
+// //                 {"name":"Relacions Catalunya-Espanya", "value":17, "legendText":"Relacions Catalunya-Espanya %", "color":"#d8d669", "url":"https://www.koolchart.com/demo/KoolChart/Assets/Images/man.svg"},
+// //                 ];
+
+// let cargarScript = (a,b) =>{
+//     var chartVars = "KoolOnLoadCallFunction=chartReadyHandler";
+//                 KoolChart.create("chart", "chartHolder", chartVars, "100%", "100%");
+//                 function chartReadyHandler(id) {
+//                 document.getElementById(id).setLayout(layoutStr);
+//                 document.getElementById(id).setData(chartData);
+//                 }
+
+//                 var layoutStr =
+//                 '<KoolChart backgroundColor="" padding="22">'
+//                     +'<Options>'
+//                     +'<Caption text=""/>'
+//                     +'</Options>'
+//                     +'<PictorialChart type="count" typeMode="form" order="sort" maxPicNumber="100" linePicNumber="20" '
+//                     +'picHGap="180" picVGap="140" direction="up" picXOffset="82" '
+//                     +'duration="1.5" textVLength="40" textPosition="down" descXOffset="300" showValueImage="false" '
+//                     +'showValue="true" valueFontSize="20" valueFontWeight="bold" valueYOffset="17" '
+//                     +'showText="true" textFontSize="14" textFontWeight="600" textColor="#333333" textXOffset="16" textYOffset="22" textAlign="left" itemHGap="16" '
+//                     +'showLegend="true" showMultiItems="true" legendTextFontSize="14" legendTextColor="#333333" legendXOffset="355" legendYOffset="11" legendGap="29" '
+//                     +'legendImageWidth="10" legendImageHeight="15" legendTextXOffset="6" legendItemHGap="15">'
+//                     +'</PictorialChart>'
+//                 +'</KoolChart>';
+
+//                 var chartData =
+//                 [{"name":"Altres", "value":a, "legendText":"Altres %", "color":"#9bd6d9", "url":"https://www.koolchart.com/demo/KoolChart/Assets/Images/man.svg"},
+//                 {"name":"Relacions Catalunya-Espanya", "value":b, "legendText":"Relacions Catalunya-Espanya %", "color":"#d8d669", "url":"https://www.koolchart.com/demo/KoolChart/Assets/Images/man.svg"},
+//                 ];
+// }
---
+++ script.js
@@ -0,0 +1,46 @@
+let mostrarModal = () =>{
+    let x = document.querySelector(".modal");
+    x.style.transform = "scale(1)";
+    x.style.opacity = "1";
+}
+
+
+let mJxC = () =>{
+    let x = document.querySelector("#preJxC");
+    x.style.display = "block";
+    let y = document.querySelector("#flo");
+    y.style.display = "none";
+
+}
+let ocJxC = () =>{
+    let x = document.querySelector("#preJxC");
+    x.style.display ="none";
+    let y = document.querySelector("#flo");
+    y.style.display = "block";
+}
+let mERC = () =>{
+    let x = document.querySelector("#preERC");
+    x.style.display = "block";
+    let y = document.querySelector("#flo");
+    y.style.display = "none";
+
+}
+let ocERC = () =>{
+    let x = document.querySelector("#preERC");
+    x.style.display ="none";
+    let y = document.querySelector("#flo");
+    y.style.display = "block";
+}
+let mCUP = () =>{
+    let x = document.querySelector("#preCUP");
+    x.style.display = "block";
+    let y = document.querySelector("#flo");
+    y.style.display = "none";
+
+}
+let ocCUP = () =>{
+    let x = document.querySelector("#preCUP");
+    x.style.display ="none";
+    let y = document.querySelector("#flo");
+    y.style.display = "block";
+}
